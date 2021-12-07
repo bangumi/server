af4488d19114a5fb887d0ba8187b1fa5898e73b2
Trim21 <trim21.me@gmail.com>
2021-12-07 19:07:38 +0800

    refactor: make db configurable

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
@@ -45,11 +48,49 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	client, err := ent.Open("mysql", "userrr:passwdd@tcp(192.168.1.3:3308)/chii", ent.Debug())
+	client, err := getEntClient()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
 
 	log.Panicln(startHTTP(client))
+}
+
+func getEntClient() (*ent.Client, error) {
+	host := getEnv("MYSQL_HOST", "127.0.0.1")
+	port := getEnv("MYSQL_PORT", "3306")
+	user := mustGetEnv("MYSQL_USER")
+	pass := mustGetEnv("MYSQL_PASS")
+	db := getEnv("MYSQL_DB", "bangumi")
+
+	debug, _ := strconv.ParseBool(getEnv("DB_DEBUG", "false"))
+
+	var options = make([]ent.Option, 0, 0)
+
+	if debug {
+		options = append(options, ent.Debug())
+	}
+
+	return ent.Open(
+		"mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, db),
+		options...,
+	)
+}
+
+func getEnv(n, v string) string {
+	if e, ok := os.LookupEnv(n); ok {
+		return e
+	}
+
+	return v
+}
+
+func mustGetEnv(n string) string {
+	if e, ok := os.LookupEnv(n); ok {
+		return e
+	}
+
+	panic("you need to set env " + strconv.Quote(n))
 }
--- readme.md
+++ readme.md
@@ -3,6 +3,17 @@
 - go 1.17+
 - GNU make
 
+## 设置
+
+可以设置以下环境变量:
+
+- `MYSQL_HOST` 默认 `127.0.0.1`
+- `MYSQL_PORT` 默认 `3306`
+- `MYSQL_DB` 默认 `bangumi`
+- `MYSQL_USER` **无默认值，必需设置**
+- `MYSQL_PASS` **无默认值，必需设置**
+- `DB_DEBUG` 是否在控制台输出所有的SQL
+
 ## 测试
 
 ```shell
