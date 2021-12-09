95b771632164e22e76133a4361b2dd46cd04d8e5
Trim21 <trim21.me+github@gmail.com>
2021-12-09 20:49:46 +0800

    fix: omit empty value from wiki parse result (#5)

--- pol/wiki/__init__.py
+++ pol/wiki/__init__.py
@@ -1,3 +1,19 @@
-from .parser import Wiki, WikiSyntaxError, parse
+from .parser import Wiki, WikiSyntaxError
+from .parser import parse as _parse
+
+
+def parse(s: str):
+    w = _parse(s)
+    if not w.info:
+        return w
+
+    result = []
+    for r in w.info:
+        if r["value"]:
+            result.append(r)
+
+    w.info = result or None
+    return w
+
 
 __all__ = ["parse", "WikiSyntaxError", "Wiki"]
