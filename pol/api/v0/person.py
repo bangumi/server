d10dab46db99d2690000087090b3d8aee22aac8c
AspectLeft <aspectleft@gmail.com>
2021-12-12 12:42:21 +0800

    fix: Add SQL filter for PersonType

--- pol/api/v0/person.py
+++ pol/api/v0/person.py
@@ -129,6 +129,9 @@ async def get_persons(
     if name is not None:
         query = query.where(ChiiPerson.prsn_name.contains(name))
 
+    if type:
+        query = query.where(ChiiPerson.prsn_type == type.value)
+
     if career_filter is not None:
         query = query.where(career_filter)
 
