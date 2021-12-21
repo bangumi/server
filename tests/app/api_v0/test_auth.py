e8e36f532dadc5a791215ac956a887a76435c543
Trim21 <trim21.me@gmail.com>
2021-12-21 23:15:05 +0800

    add test

--- tests/app/api_v0/test_auth.py
+++ tests/app/api_v0/test_auth.py
@@ -1,7 +1,17 @@
 from starlette.testclient import TestClient
 
+access_token = "a_development_access_token"
 
-def test_auth_401(client: TestClient):
-    response = client.get("/v0/episodes/10000000")
-    assert response.status_code == 404
+
+def test_auth_200(client: TestClient):
+    response = client.get("/v0/me", headers={"Authorization": f"Bearer {access_token}"})
+    assert response.status_code == 200
     assert response.headers["content-type"] == "application/json"
+
+
+def test_auth_403(client: TestClient):
+    response = client.get("/v0/me", headers={"Authorization": "Bearer "})
+    assert response.status_code == 403, "no token"
+
+    response = client.get("/v0/me", headers={"Authorization": f"t {access_token}"})
+    assert response.status_code == 403, "no token"
