2a005d3ffd34006c8c431e279adbc613d81b9cec
Trim21 <trim21.me@gmail.com>
2021-12-29 04:52:52 +0800

    remove unused code

--- pol/headers/cache.py
+++ pol/headers/cache.py
@@ -5,9 +5,6 @@ class CacheHeader:
     def __init__(self, response: Response):
         self.response = response
 
-    def __call__(self):
-        return self
-
     def public(self, second: int):
         self.response.headers["Cache-Control"] = f"public, max-age={second}"
 
