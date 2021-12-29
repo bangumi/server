d551ce762cbc28e05a30b34e4597e03da3fe82f7
Trim21 <trim21.me@gmail.com>
2021-12-30 03:31:33 +0800

    split character type and person role

--- pol/db/const.py
+++ pol/db/const.py
@@ -20,11 +20,20 @@ class BloodType(enum.IntEnum):
         return self.value
 
 
-class PersonType(enum.IntEnum):
+class CharacterType(enum.IntEnum):
     person = 1
     company = 2
     band = 3
     group = 4
+
+    def translate(self, _escape_table):
+        return self.value
+
+
+class PersonType(enum.IntEnum):
+    person = 1
+    company = 2
+    band = 3
 
     def translate(self, _escape_table):
         return self.value
