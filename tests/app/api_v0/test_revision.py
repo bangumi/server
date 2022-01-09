d88feb67724e071e7d2d22f7fcea77573b73c18c
Trim21 <trim21.me@gmail.com>
2022-01-09 15:56:32 +0800

    remove cache

--- tests/app/api_v0/test_revision.py
+++ tests/app/api_v0/test_revision.py
@@ -1,4 +1,3 @@
-import redis
 from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
 
@@ -35,30 +34,6 @@ def test_person_revision_basic(
     assert res["data"]["348475"]["prsn_name"] == "森岡浩之"
 
 
-def test_person_revision_cache(
-    client: TestClient,
-    redis_client: redis.Redis,
-    db_session: Session,
-    mock_user: MockUser,
-):
-    for r in db_session.query(ChiiRevHistory.rev_creator).where(
-        ChiiRevHistory.rev_id == 348475
-    ):
-        mock_user(r.rev_creator)
-    url = f"{person_revisions_api_prefix}/348475"
-    response = client.get(
-        url,
-    )
-    assert response.status_code == 200
-    assert response.headers["x-cache-status"] == "miss"
-
-    response = client.get(
-        url,
-    )
-    assert response.headers["x-cache-status"] == "hit"
-    assert response.status_code == 200
-
-
 character_revisions_api_prefix = "/v0/revisions/characters"
 
 
@@ -87,27 +62,3 @@ def test_character_revision_basic(
         "creator",
     }
     assert res["data"]["1"]["crt_name"] == "ルルーシュ・ランペルージ"
-
-
-def test_character_revision_cache(
-    client: TestClient,
-    redis_client: redis.Redis,
-    db_session: Session,
-    mock_user: MockUser,
-):
-    for r in db_session.query(ChiiRevHistory.rev_creator).where(
-        ChiiRevHistory.rev_id == 190704
-    ):
-        mock_user(r.rev_creator)
-    url = f"{character_revisions_api_prefix}/190704"
-    response = client.get(
-        url,
-    )
-    assert response.status_code == 200
-    assert response.headers["x-cache-status"] == "miss"
-
-    response = client.get(
-        url,
-    )
-    assert response.headers["x-cache-status"] == "hit"
-    assert response.status_code == 200
