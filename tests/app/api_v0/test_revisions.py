11d4146b2cd41b435af4020992242152d40ad303
MichaelMai2000 <maitiancheng@qq.com>
2022-01-11 17:59:23 +1100

    chore(revision): add tests

--- tests/app/api_v0/test_revisions.py
+++ tests/app/api_v0/test_revisions.py
@@ -138,3 +138,101 @@ def test_character_revisions_page_limit(
         character_revisions_api_prefix, params={"charater_id": 1, "offset": offset}
     )
     assert response.status_code == 422, response.text
+
+
+subject_revisions_api_prefix = "/v0/revisions/subjects"
+
+
+def test_subject_revisions_basic(
+    client: TestClient,
+):
+    response = client.get(subject_revisions_api_prefix, params={"subject_id": 1})
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+
+    res = response.json()
+    assert res["total"]
+    assert res["offset"] == 0
+    assert "limit" in res
+    for item in res["data"]:
+        assert "nickname" in item["creator"]
+
+
+def test_subject_revisions_offset(
+    client: TestClient,
+):
+    offset = 1
+    common_params = {"subject_id": 1}
+    response1 = client.get(
+        subject_revisions_api_prefix, params={"offset": offset, **common_params}
+    )
+    assert response1.status_code == 200
+    assert response1.headers["content-type"] == "application/json"
+
+    res = response1.json()
+    assert (
+        res["data"][0]["id"]
+        == client.get(subject_revisions_api_prefix, params=common_params).json()[
+            "data"
+        ][1]["id"]
+    )
+    assert res["offset"] == offset
+
+
+def test_subject_revisions_page_limit(
+    client: TestClient,
+):
+    offset = 30000
+    response = client.get(
+        subject_revisions_api_prefix, params={"subject_id": 1, "offset": offset}
+    )
+    assert response.status_code == 422, response.text
+
+
+episode_revisions_api_prefix = "/v0/revisions/episodes"
+
+
+def test_episode_revisions_basic(
+    client: TestClient,
+):
+    response = client.get(episode_revisions_api_prefix, params={"episode_id": 522})
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
+
+    res = response.json()
+    assert res["total"]
+    assert res["offset"] == 0
+    assert "limit" in res
+    for item in res["data"]:
+        assert "nickname" in item["creator"]
+
+
+def test_episode_revisions_offset(
+    client: TestClient,
+):
+    offset = 1
+    common_params = {"episode_id": 1045}
+    response1 = client.get(
+        episode_revisions_api_prefix, params={"offset": offset, **common_params}
+    )
+    assert response1.status_code == 200
+    assert response1.headers["content-type"] == "application/json"
+
+    res = response1.json()
+    assert (
+        res["data"][0]["id"]
+        == client.get(episode_revisions_api_prefix, params=common_params).json()[
+            "data"
+        ][1]["id"]
+    )
+    assert res["offset"] == offset
+
+
+def test_episode_revisions_page_limit(
+    client: TestClient,
+):
+    offset = 30000
+    response = client.get(
+        episode_revisions_api_prefix, params={"episode_id": 522, "offset": offset}
+    )
+    assert response.status_code == 422, response.text
