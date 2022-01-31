7b6a747b759c56ec08ea0b947aab2b12c85d3499
Trim21 <i@trim21.me>
2022-01-31 19:28:26 +0800

    tests: aviod params mess up output

--- pol/api/v0/utils_test.py
+++ pol/api/v0/utils_test.py
@@ -1,15 +1,13 @@
-import pytest
+import difflib
 
 from pol.api.v0.utils import short_description
 
 
-@pytest.mark.parametrize(
-    ("input", "expected"),
-    [
+def test_short_description():
+    for input, expected in [
         ("=" * 100, "=" * 77 + "..."),
         ("==", "=="),
         ("=" * 79, "=" * 79),
-    ],
-)
-def test_short_description(input: str, expected: str):
-    assert short_description(input) == expected
+    ]:
+        actual = short_description(input)
+        assert actual == expected, difflib.context_diff(actual, expected)
