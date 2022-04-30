f7e642b96c5ae82c81eadd4f6c6ba48076dbc015
Trim21 <trim21.me@gmail.com>
2022-05-01 05:45:39 +0800

    check content-type json as prefix

--- web/req/content_type.go
+++ web/req/content_type.go
@@ -25,10 +25,10 @@ import (
 	"github.com/bangumi/server/web/res"
 )
 
-var jsonType = []byte(fiber.MIMEApplicationJSON) //nolint:gochecknoglobals
+var jsonType = []byte("application/json") //nolint:gochecknoglobals
 
 func JSON(c *fiber.Ctx) error {
-	if bytes.Equal(c.Request().Header.ContentType(), jsonType) {
+	if bytes.HasPrefix(c.Request().Header.ContentType(), jsonType) {
 		return res.HTTPError(c, http.StatusBadRequest, "need content-type to be 'application/json'")
 	}
 
