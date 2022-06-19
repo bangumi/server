cd7c76486633c2917fed1e8cc9aa4333a27faaa4
Trim21 <trim21.me+github@gmail.com>
2022-06-20 05:44:11 +0800

    Update openapi/bundle.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- openapi/bundle.js
+++ openapi/bundle.js
@@ -7,7 +7,7 @@ const lodash = require("lodash");
 async function main() {
   let schema = await $RefParser.bundle(process.argv[2]);
 
-  schema = lodash.omit(schema, "x-parameters")
+  schema = lodash.omit(schema, "x-parameters");
 
   if (process.argv[3]) {
     await fs.writeFile(process.argv[3], yaml.dump(schema, {noRefs: true}));
