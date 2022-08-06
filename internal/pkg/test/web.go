4fc2f294b351979d97ab6d031d5ba514ff3bb4ac
Trim21 <trim21.me@gmail.com>
2022-08-06 15:32:05 +0800

    fix

--- internal/pkg/test/web.go
+++ internal/pkg/test/web.go
@@ -115,6 +115,7 @@ func GetWebApp(tb testing.TB, m Mock) *fiber.App {
 
 		// this will transform t.FailNow()
 		// to panic
+		// until https://github.com/gofiber/fiber/issues/1996
 		fx.Invoke(func(app *fiber.App) {
 			app.Use(func(c *fiber.Ctx) error {
 				var returned bool
