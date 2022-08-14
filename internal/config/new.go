ca4a4c3f66f737012886e74a8524a4b401a8e4fd
Trim21 <trim21.me@gmail.com>
2022-08-14 14:32:31 +0800

    revert

--- internal/config/new.go
+++ internal/config/new.go
@@ -29,14 +29,13 @@ import (
 type AppConfig struct {
 	Debug map[string]bool `yaml:"debug"`
 
-	RedisURL            string `yaml:"redis_url" env:"REDIS_URI" env-default:"redis://127.0.0.1:6379/0"`
-	MySQLHost           string `yaml:"mysql_host" env:"MYSQL_HOST" env-default:"127.0.0.1"`
-	MySQLPort           string `yaml:"mysql_port" env:"MYSQL_PORT" env-default:"3306"`
-	MySQLUserName       string `yaml:"mysql_user" env:"MYSQL_USER" env-default:"user"`
-	MySQLBinlogServerID int    `yaml:"mysql_slave_id" env:"MYSQL_SLAVE_ID" env-default:"4"`
-	MySQLPassword       string `yaml:"mysql_pass" env:"MYSQL_PASS" env-default:"password"`
-	MySQLDatabase       string `yaml:"mysql_db" env:"MYSQL_DB" env-default:"bangumi"`
-	MySQLMaxConn        int    `yaml:"mysql_max_connection" env:"MYSQL_MAX_CONNECTION" env-default:"4"`
+	RedisURL      string `yaml:"redis_url" env:"REDIS_URI" env-default:"redis://127.0.0.1:6379/0"`
+	MySQLHost     string `yaml:"mysql_host" env:"MYSQL_HOST" env-default:"127.0.0.1"`
+	MySQLPort     string `yaml:"mysql_port" env:"MYSQL_PORT" env-default:"3306"`
+	MySQLUserName string `yaml:"mysql_user" env:"MYSQL_USER" env-default:"user"`
+	MySQLPassword string `yaml:"mysql_pass" env:"MYSQL_PASS" env-default:"password"`
+	MySQLDatabase string `yaml:"mysql_db" env:"MYSQL_DB" env-default:"bangumi"`
+	MySQLMaxConn  int    `yaml:"mysql_max_connection" env:"MYSQL_MAX_CONNECTION" env-default:"4"`
 
 	WebDomain string `yaml:"web_domain" env:"WEB_DOMAIN"` // new frontend web page domain
 	HTTPHost  string `yaml:"http_host" env:"HTTP_HOST" env-default:"127.0.0.1"`
