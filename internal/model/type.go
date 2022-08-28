dde5f83330536b6ec1fece2f4c38526cdec93b01
TWT <TWT2333@outlook.com>
2022-08-28 22:44:13 +0800

    2244

--- internal/model/type.go
+++ internal/model/type.go
@@ -71,6 +71,7 @@ func (v GroupID) Value() (driver.Value, error) {
 
 func (v TimeLineID) Value() (driver.Value, error) {
 	return int64(v), nil
+}
 
 func (v GroupID) Zap() zap.Field {
 	return zap.Uint32("group_id", uint32(v))
