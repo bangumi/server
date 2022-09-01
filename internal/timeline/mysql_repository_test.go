134a0f3a3c32bbe5eb738e422b94ab0962f5fa95
Trim21 <trim21.me@gmail.com>
2022-09-01 21:43:56 +0800

    type style

--- internal/timeline/mysql_repository_test.go
+++ internal/timeline/mysql_repository_test.go
@@ -16,6 +16,7 @@ package timeline_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -38,10 +39,11 @@ func getRepo(t *testing.T) (domain.TimeLineRepo, *query.Query) {
 }
 
 func Test_mysqlRepo_GetByID(t *testing.T) {
-	var tlID model.TimeLineID = 28979826
-
 	test.RequireEnv(t, test.EnvMysql)
 	t.Parallel()
+
+	var tlID model.TimeLineID = 28979826
+
 	repo, q := getRepo(t)
 	ctx := context.Background()
 
@@ -57,14 +59,15 @@ func Test_mysqlRepo_GetByID(t *testing.T) {
 }
 
 func Test_mysqlRepo_ListByUID(t *testing.T) {
+	test.RequireEnv(t, test.EnvMysql)
+	t.Parallel()
+
 	var (
 		uid   model.UserID = 287622
 		limit              = -1
 		since model.TimeLineID
 	)
 
-	test.RequireEnv(t, test.EnvMysql)
-	t.Parallel()
 	repo, q := getRepo(t)
 	ctx := context.Background()
 
@@ -80,56 +83,54 @@ func Test_mysqlRepo_ListByUID(t *testing.T) {
 }
 
 func Test_mysqlRepo_Create(t *testing.T) {
-	var (
-		cases = []model.TimeLineID{
-			28684877, // cat=1, type=2
-			28682314, // cat=1, type=3
-			28683701, // cat=2
-			28685055, // cat=3
-			28976108, // cat=4
-			28684294, // cat=5, type=2
-			28683198, // cat=5, type=1
-			28684975, // cat=6
-			28684740, // cat=7
-			28685042, // cat=8
-			28523860, // cat=9
-		}
-	)
-
 	test.RequireEnv(t, test.EnvMysql)
 	t.Parallel()
+
+	var cases = []model.TimeLineID{
+		28684877, // cat=1, type=2
+		28682314, // cat=1, type=3
+		28683701, // cat=2
+		28685055, // cat=3
+		28976108, // cat=4
+		28684294, // cat=5, type=2
+		28683198, // cat=5, type=1
+		28684975, // cat=6
+		28684740, // cat=7
+		28685042, // cat=8
+		28523860, // cat=9
+	}
+
 	repo, q := getRepo(t)
 	ctx := context.Background()
 
-	once := func(tlID model.TimeLineID) {
-		newTLID := tlID + 987654321
-
-		// delete if already exists
-		_, err := q.WithContext(ctx).TimeLine.Where(q.TimeLine.ID.Eq(newTLID)).Delete()
-		require.NoError(t, err)
-
-		// get the timeline
-		tlModel, err := repo.GetByID(ctx, tlID)
-		require.NoError(t, err)
-
-		// alter id and uid
-		tlModel.ID = newTLID
-		tlModel.UID += 654321
-
-		// create with new id
-		err = repo.Create(ctx, tlModel)
-		require.NoError(t, err)
-
-		// get new timeline
-		newTLModel, err := repo.GetByID(ctx, newTLID)
-		require.NoError(t, err)
-
-		// check if the new timeline eq old timeline
-		require.Equal(t, tlModel, newTLModel)
-	}
-
 	for _, tlID := range cases {
-		t.Log("start testing case:", tlID)
-		once(tlID)
+		tlID := tlID
+		t.Run(fmt.Sprintf("start testing case: %d", tlID), func(t *testing.T) {
+			t.Parallel()
+			newTLID := tlID + 987654321
+
+			// delete if already exists
+			_, err := q.WithContext(ctx).TimeLine.Where(q.TimeLine.ID.Eq(newTLID)).Delete()
+			require.NoError(t, err)
+
+			// get the timeline
+			tlModel, err := repo.GetByID(ctx, tlID)
+			require.NoError(t, err)
+
+			// alter id and uid
+			tlModel.ID = newTLID
+			tlModel.UID += 654321
+
+			// create with new id
+			err = repo.Create(ctx, tlModel)
+			require.NoError(t, err)
+
+			// get new timeline
+			newTLModel, err := repo.GetByID(ctx, newTLID)
+			require.NoError(t, err)
+
+			// check if the new timeline eq old timeline
+			require.Equal(t, tlModel, newTLModel)
+		})
 	}
 }
