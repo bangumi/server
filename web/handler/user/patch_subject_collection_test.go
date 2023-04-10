3a08abe822e9a4ee3083b48c57111ce619897537
RanKKI <youloveonlymeh@gmail.com>
2023-04-10 22:11:17 +1000

    test(collection): Add test case for Patch
    Patching to a non-existent subject collection in User_PatchSubjectCollection in patch_subject_collection_test.go

--- web/handler/user/patch_subject_collection_test.go
+++ web/handler/user/patch_subject_collection_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/trim21/htest"
 
 	"github.com/bangumi/server/config"
+	"github.com/bangumi/server/domain/gerr"
 	"github.com/bangumi/server/internal/auth"
 	"github.com/bangumi/server/internal/collections/domain/collection"
 	"github.com/bangumi/server/internal/mocks"
@@ -83,6 +84,38 @@ func TestUser_PatchSubjectCollection(t *testing.T) {
 	require.Equal(t, "1 test_content 2", s.Comment())
 	require.EqualValues(t, []string{"q", "vv"}, s.Tags())
 	require.EqualValues(t, 8, s.Rate())
+}
+
+func TestUser_PatchToNonExistsSubjectCollection(t *testing.T) {
+	t.Parallel()
+	const sid model.SubjectID = 8
+	const uid model.UserID = 1
+
+	a := mocks.NewAuthService(t)
+	a.EXPECT().GetByToken(mock.Anything, mock.Anything).Return(auth.Auth{ID: uid}, nil)
+
+	tl := mocks.NewTimeLineService(t)
+
+	c := mocks.NewCollectionRepo(t)
+	c.EXPECT().UpdateSubjectCollection(mock.Anything, uid, sid, mock.Anything, mock.Anything, mock.Anything).
+		Return(gerr.ErrSubjectNotCollected)
+
+	d, err := dam.New(config.AppConfig{NsfwWord: "", DisableWords: "test_content", BannedDomain: ""})
+	require.NoError(t, err)
+
+	app := test.GetWebApp(t, test.Mock{CollectionRepo: c, AuthService: a, Dam: &d, TimeLineSrv: tl})
+
+	htest.New(t, app).
+		Header(echo.HeaderAuthorization, "Bearer t").
+		BodyJSON(map[string]any{
+			"comment": "1 test_content 2",
+			"type":    1,
+			"private": true,
+			"rate":    8,
+			"tags":    []string{"q", "vv"},
+		}).
+		Patch(fmt.Sprintf("/v0/users/-/collections/%d", sid)).
+		ExpectCode(http.StatusNotFound)
 }
 
 func TestUser_PatchSubjectCollection_bad(t *testing.T) {
