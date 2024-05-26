b8412b31547629ae91b5d57e0f7c24649a8ccb42
everpcpc <git@everpcpc.com>
2024-05-26 22:31:04 +0800

    z

--- internal/subject/mysql_repository.go
+++ internal/subject/mysql_repository.go
@@ -234,7 +234,7 @@ func (r mysqlRepo) GetByIDs(
 func (r mysqlRepo) Browse(
 	ctx context.Context, filter BrowseFilter, limit, offset int,
 ) ([]model.Subject, error) {
-	// TODO:
+	// TODO: everpcpc
 	return []model.Subject{}, nil
 }
 
