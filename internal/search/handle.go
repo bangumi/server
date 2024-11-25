867e0f06083a2b8b014c842accffb778676d1997
Trim21 <trim21.me@gmail.com>
2024-11-26 00:38:07 +0800

    perf: only get meta tags for non-empty string

--- internal/search/handle.go
+++ internal/search/handle.go
@@ -135,10 +135,17 @@ func (c *client) Handle(ctx echo.Context) error {
 		return errgo.Wrap(err, "subjectRepo.GetByIDs")
 	}
 
-	// tags, err := c.tagRepo.GetByIDs(ctx.Request().Context(), ids)
-	// if err != nil {
-	// 	return errgo.Wrap(err, "tagRepo.GetByIDs")
-	// }
+	var idsToGetTags []model.SubjectID
+	for _, m := range subjects {
+		if m.MetaTags != "" {
+			idsToGetTags = append(idsToGetTags, m.ID)
+		}
+	}
+
+	tags, err := c.tagRepo.GetByIDs(ctx.Request().Context(), idsToGetTags)
+	if err != nil {
+		return errgo.Wrap(err, "tagRepo.GetByIDs")
+	}
 
 	var data = make([]ReponseSubject, 0, len(subjects))
 	for _, id := range ids {
@@ -146,8 +153,10 @@ func (c *client) Handle(ctx echo.Context) error {
 		if !ok {
 			continue
 		}
-		// metaTags := tags[id]
-		var metaTags []tag.Tag
+		metaTags := tags[id]
+		if metaTags == nil {
+			metaTags = []tag.Tag{}
+		}
 
 		for _, t := range strings.Split(s.MetaTags, " ") {
 			metaTags = append(metaTags, tag.Tag{Name: t, Count: 1})
