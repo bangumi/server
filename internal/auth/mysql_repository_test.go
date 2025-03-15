d0b491f45eb7b46ab56d032f1e10539c8b816e38
Trim21 <trim21.me@gmail.com>
2025-03-15 23:04:32 +0800

    refactor: remove dead code

--- internal/auth/mysql_repository_test.go
+++ internal/auth/mysql_repository_test.go
@@ -16,21 +16,17 @@ package auth_test
 
 import (
 	"context"
-	"strconv"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
-	"github.com/bangumi/server/dal/dao"
 	"github.com/bangumi/server/dal/query"
 	"github.com/bangumi/server/domain/gerr"
 	"github.com/bangumi/server/internal/auth"
-	"github.com/bangumi/server/internal/pkg/gtime"
 	"github.com/bangumi/server/internal/pkg/test"
 )
 
@@ -82,78 +78,4 @@ func TestMysqlRepo_GetByToken_expired(t *testing.T) {
 
 	_, err := repo.GetByToken(context.Background(), "a_expired_token")
 	require.ErrorIs(t, err, gerr.ErrNotFound)
-}
-
-func TestMysqlRepo_CreateAccessToken(t *testing.T) {
-	test.RequireEnv(t, "mysql")
-	t.Parallel()
-
-	repo, q := getRepo(t)
-	t.Cleanup(func() {
-		_, err := q.AccessToken.WithContext(context.TODO()).Where(q.AccessToken.UserID.Eq("1")).Delete()
-		require.NoError(t, err)
-	})
-
-	token, err := repo.CreateAccessToken(context.Background(), 1, "token name", gtime.OneWeek)
-	require.NoError(t, err)
-	require.Len(t, token, 40)
-}
-
-func TestMysqlRepo_DeleteAccessToken(t *testing.T) {
-	test.RequireEnv(t, "mysql")
-	t.Parallel()
-
-	const id = 100
-	repo, q := getRepo(t)
-
-	cleanup := func() {
-		_, err := q.AccessToken.WithContext(context.TODO()).Where(q.AccessToken.ID.Eq(id)).Delete()
-		require.NoError(t, err)
-	}
-	t.Cleanup(cleanup)
-
-	err := q.AccessToken.WithContext(context.Background()).Create(&dao.AccessToken{
-		ID:          id,
-		Type:        auth.TokenTypeAccessToken,
-		AccessToken: t.Name(),
-		ClientID:    "access token",
-		UserID:      "2",
-		ExpiredAt:   time.Now().Add(gtime.OneWeek),
-		Scope:       nil,
-		Info:        []byte{},
-	})
-	require.NoError(t, err)
-
-	ok, err := repo.DeleteAccessToken(context.Background(), id)
-	require.NoError(t, err)
-	require.True(t, ok)
-}
-
-func TestMysqlRepo_ListAccessToken(t *testing.T) {
-	test.RequireEnv(t, "mysql")
-	t.Parallel()
-
-	repo, q := getRepo(t)
-
-	test.RunAndCleanup(t, func() {
-		_, err := q.AccessToken.WithContext(context.TODO()).Where(q.AccessToken.UserID.Eq("3")).Delete()
-		require.NoError(t, err)
-	})
-
-	for i := 1; i < 5; i++ {
-		err := q.AccessToken.WithContext(context.Background()).Create(&dao.AccessToken{
-			Type:        auth.TokenTypeAccessToken,
-			AccessToken: t.Name() + strconv.Itoa(i),
-			ClientID:    "access token",
-			UserID:      "3",
-			ExpiredAt:   time.Now().Add(gtime.OneWeek),
-			Scope:       nil,
-			Info:        []byte{},
-		})
-		require.NoError(t, err)
-	}
-
-	tokens, err := repo.ListAccessToken(context.Background(), 3)
-	require.NoError(t, err)
-	require.Len(t, tokens, 4)
 }
