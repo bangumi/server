69cb07481e7aca27163e3c60619269c9a33381d5
Trim21 <trim21.me@gmail.com>
2025-05-10 08:00:49 +0800

    fix: music subject platform warning

--- scripts/build-common.mjs
+++ scripts/build-common.mjs
@@ -1,14 +1,23 @@
-import * as path from "node:path";
 import * as fs from "node:fs";
 
 import * as yaml from "yaml";
 
 const data = yaml.parse(fs.readFileSync("pkg/vars/common/subject_platforms.yml", "utf-8"));
 
-fs.writeFileSync(
-  "pkg/vars/platform.go.json",
-  JSON.stringify(Object.fromEntries(Object.entries(data.platforms).filter(([key, value]) => /\d/.test(key))), null, 2),
-);
+const platforms = Object.fromEntries(Object.entries(data.platforms).filter(([key, value]) => /\d/.test(key)));
+
+platforms[3] ??= {
+  0: {
+    id: 0,
+    type: "",
+    type_cn: "",
+    alias: "",
+    wiki_tpl: "",
+    order: 0,
+  },
+};
+
+fs.writeFileSync("pkg/vars/platform.go.json", JSON.stringify(platforms, null, 2));
 
 fs.writeFileSync(
   "pkg/vars/staffs.go.json",
