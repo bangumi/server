88e22788e244adb7181556e3ddcdc5459e532b31
Trim21 <trim21.me@gmail.com>
2026-03-19 00:12:14 +0800

    fix: unescape person and character wiki fields in archive (#1031)

---
+++ internal/character/mysql_repository_internal_test.go
@@ -0,0 +1,35 @@
+package character
+
+import (
+	"html"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bangumi/server/dal/dao"
+	"github.com/bangumi/server/dal/utiltype"
+)
+
+func TestConvertDao_UnescapesHTMLStrings(t *testing.T) {
+	t.Parallel()
+
+	wikiValue := `[nickname|"First Hassan"] Tom & Jerry <tag> 'quote'`
+	nameValue := `Tom & Jerry <The "Best">`
+
+	character := convertDao(&dao.Character{
+		Name:    mustScanHTMLString(t, html.EscapeString(nameValue)),
+		Infobox: mustScanHTMLString(t, html.EscapeString(wikiValue)),
+	})
+
+	require.Equal(t, nameValue, character.Name)
+	require.Equal(t, wikiValue, character.Infobox)
+}
+
+func mustScanHTMLString(t *testing.T, value string) utiltype.HTMLEscapedString {
+	t.Helper()
+
+	var result utiltype.HTMLEscapedString
+	require.NoError(t, (&result).Scan(value))
+
+	return result
+}
